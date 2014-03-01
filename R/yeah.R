18c4e02cf9ec06af3a65d25deb8bc0b5b0d6b37e
dichika <ichikawa@hcc-jp.com>
2014-03-02 00:33:47 +0900

    initial commit

---
+++ R/yeah.R
@@ -0,0 +1,8 @@
+yeah <- function(pkgname){
+  require(pingr)
+  sound_path <- system.file("sounds/yeah.wav", package=pkgname)
+  if(!(nchar(sound_path)>0)){
+    stop("\nHmm...this package doesn't have no yeah.")
+  }
+  ping2(path=sound_path)
+}
