d4a47d89c6ade7b052f220fc54285b24e20b6b17
dichika <ichikawa@hcc-jp.com>
2014-03-11 19:34:09 +0900

    fix null handling

--- R/yeah.R
+++ R/yeah.R
@@ -1,6 +1,9 @@
-yeah <- function(pkgname){
+yeah <- function(pkgname=NULL){
   require(pingr)
-  sound_path <- system.file("sounds/yeah.wav", package=pkgname)
+  sound_path <- ifelse(is.null(pkgname), 
+                       system.file("sounds/yeah.wav", package="yeah"),
+                       system.file("sounds/yeah.wav", package=pkgname)
+  )
   if(!(nchar(sound_path)>0)){
     stop("\nHmm...this package doesn't have no yeah.")
   }
