b5548d3707a0a49803215b713504da54ad4f9cce
市川太祐 <ichikawadaisuke@ichikawafutoshiyuu-no-MacBook-Air.local>
2014-11-30 20:35:27 +0900

    add new function and files

--- R/yeah.R
+++ R/yeah.R
@@ -1,5 +1,4 @@
 yeah <- function(pkgname=NULL){
-  require(pingr)
   sound_path <- ifelse(is.null(pkgname), 
                        system.file("sounds/yeah.wav", package="yeah"),
                        system.file("sounds/yeah.wav", package=pkgname)
@@ -7,11 +6,16 @@ yeah <- function(pkgname=NULL){
   if(!(nchar(sound_path)>0)){
     stop("\nHmm...this package doesn't have no yeah.")
   }
-  ping(sound_path)
+  beep(sound_path)
 }
 
 libraryy <- function(package=NULL,...){
   pkg <- as.character(substitute(package))
   library(package=pkg, character.only = TRUE, ...)
   yeah(pkg)
+}
+
+detayo <- function(){
+  path <- system.file("sounds/yuuisa.wav", package="yeah")
+  beep(path)
 }
