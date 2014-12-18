6e37db5dec580ecca1aa48d6cf7f9c171dd0d664
dichika <ichikawa@hcc-jp.com>
2014-12-18 11:53:28 +0900

    getKey関連

---
+++ inst/ruby/hook.rb
@@ -0,0 +1,23 @@
+require 'java'
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+class KeyHook
+  include NativeKeyListener
+  def nativeKeyPressed(e)
+    code =  e.getKeyCode()
+    if code == 28 then
+      system("afplay " + Dir.glob(ARGV[0]+'*').choice)
+    elsif code == 1 then
+      java.lang.System.exit(0)
+    end
+  end
+  def nativeKeyReleased(e)
+  end
+  def nativeKeyTyped(e)
+  end
+end
+
+GlobalScreen.registerNativeHook
+GlobalScreen.getInstance.addNativeKeyListener(KeyHook.new)
